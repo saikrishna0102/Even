e578bea8accdaba5a27b3f2e80493e778e474037
saikrishna0102 <kancharlasai49@gmail.com>
2023-09-25 10:41:57 +0530

    adding class

---
+++ src/even/java/Even.java
@@ -0,0 +1,21 @@
+package even.java;
+
+public class Even {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int n=100, i=1;  
+	        System.out.print("List of even numbers from 1 to 100: \n");  
+	        while (i<=n)   
+	        { 
+	            if ( i%2 == 0 )
+	            {
+	                System.out.print(i + " ");
+	            }
+	            i++;
+	        }
+	    }  
+	
+	}
+
+
